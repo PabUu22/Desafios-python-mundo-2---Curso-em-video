9ec7b2359642e64ad1a1b3d8e226814209ade946
Pablo Vinícius <viniciusdesousaa20@gmail.com>
2022-08-31 01:32:14 -0300

    Update Desafio042.py

--- Desafios mundo 2 python/Desafio042.py
+++ Desafios mundo 2 python/Desafio042.py
@@ -1,2 +1,20 @@
 # Refaça o DESAFIO 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
 
+a = float(input('Primeiro segmento: '))
+b = float(input('Segundo segmento: '))
+c = float(input('Terceiro segmento: '))
+
+if a < (b + c) and b < (a + c) and c < (a + b):
+    print('os segmentos podem formar um triangulo ', end="" )
+    if a == b == c:
+        print("Equilátero")
+    elif a != b != c != a:
+        print("Escaleno")
+    else:
+        print("Isósceles")
+else:
+    print('os segmentos não podem formar um triangulo')
+
+
+
+
