08e0f8c142d55d75498933139eeb4497ec15db48
Pablo Vinícius <viniciusdesousaa20@gmail.com>
2022-08-31 10:49:26 -0300

    Update Desafio043.py

--- Desafios mundo 2 python/Desafio043.py
+++ Desafios mundo 2 python/Desafio043.py
@@ -11,9 +11,9 @@ if imc < 18.5:
     print("Você está abaixo do peso")
 elif imc > 18.5 and imc < 25:
     print("Você está no peso ideal")
-elif imc > 25 and imc < 30:
+elif imc >= 25 and imc < 30:
     print("Você está sobrepeso")
-elif imc > 30 and imc < 40:
+elif imc >= 30 and imc < 40:
     print("Você está com obsidade")
-elif imc > 40:
+elif imc >= 40:
     print("Você está com obsidade morbida")
